376a6330f4a04fc5b314c24a3046742a224f4eda
ArtHome12 <_mag12@yahoo.com>
2020-02-04 23:23:53 +0700

    PWM level

--- dimmable_led.ino
+++ dimmable_led.ino
@@ -7,16 +7,21 @@ http://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2020 by Artem Khomenko _mag12@yahoo.com.
 =============================================================================== */
 
-#include <Bounce2.h>;
+#include <Bounce2.h>
+#include <EEPROM.h>
 
 // задаем константы
-const int buttonPin = 2;     // номер входа, подключенный к кнопке
-const int ledPin =  13;      // номер выхода светодиода
+const int buttonPin = 2;  // номер входа, подключенный к кнопке
+const int ledPin =  13;   // номер выхода светодиода
+const int driverPin = 3;  // номер выхода на драйвер (транзистор).    
  
-// Команды
-const int bClick = 1;         // отжатие после щелчка
-const int bLongPress = 2;     // долгое нажатие
-const int bDobleClick = 3;    // двойной щелчок
+// Поддерживаемые события
+enum Events {
+  eClick,         // отжатие после щелчка
+  eLongPress,     // долгое нажатие
+  eLongPressFree, // отпуск после долгого нажатия
+  eDoubleClick    // двойной щелчок
+};
 
 unsigned long previousMillis = 0;         // Момент последнего обновления
 const unsigned long maxClickDelay = 500;  // Максимальное время для короткого щелчка, мс.
@@ -25,6 +30,10 @@ const unsigned long longPressRepeatDelay = 50; // Время между повт
 unsigned long previousClickMillis = 0;    // Время последнего короткого щелчка (для генерации двойного), мс.
 
 bool isBPressed = false;                // Истина, когда кнопка нажата.  
+
+const int eepromAddrShim = 0;       // Адрес для хранения в EEPROM признака завершения работы.
+byte PWMLevel = 0;                  // Текущий уровень на ШИМ
+bool increaseUp = true;             // Направление увеличения яркости при повторении LongPress.
 
 // Для подавления дребезга
 Bounce debouncer = Bounce();
@@ -38,7 +47,8 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
 
-  //previousMillis = millis();
+  // Прочитаем прежнее значение яркости из EEPROM
+  PWMLevel = EEPROM.read(eepromAddrShim);
 }
  
 void loop(){
@@ -64,19 +74,22 @@ void loop(){
       // Если кнопка была отжата ранее наступления интервала, произошёл щелчок.
       if (deltaTime < maxClickDelay) {
         // Отправляем событие либо щелчок либо двойной щелчок.
-        event(currentMillis - previousClickMillis < longPressFirstDelay ? bDobleClick : bClick);
+        event(currentMillis - previousClickMillis < longPressFirstDelay ? eDoubleClick : eClick);
 
         // Сохраняем время.
         previousClickMillis = currentMillis;
-      }
+      } else {
+        // Если кнопка была отжата после долговременного нажатия.
+        event(eLongPressFree);
+      } 
     } 
   } else {
     // Если состояние не менялось.
 
-    // Если кнопка в нажатом состоянии, и прошло достаточно времени, надо посылать долгое нажатие.
+    // Если кнопка в нажатом состShimоянии, и прошло достаточно времени, надо посылать долгое нажатие.
     if (isBPressed) { 
       if (deltaTime > longPressFirstDelay) {
-        event(bLongPress);
+        event(eLongPress);
 
         // Откорректируем время так, чтобы повторные длинные нажатия происходили быстро.
         previousMillis = currentMillis - longPressFirstDelay + longPressRepeatDelay;
@@ -90,6 +103,43 @@ void loop(){
   digitalWrite(ledPin, isBPressed ? HIGH : LOW);
 }
 
-void event(int bCommand) {
-  Serial.println(bCommand);
+void event(Events bCommand) {
+  switch (bCommand)
+  {
+  case eClick:
+    Serial.println("Click");
+    break;
+  
+  case eLongPress:
+    Serial.print("PWM Level ");
+    Serial.println(int(PWMLevel));
+
+    // Изменяем значение уровня.
+    if (increaseUp) {
+      if (PWMLevel == 255) {
+        PWMLevel = 254;
+        increaseUp = false;
+      } else {
+          PWMLevel++;
+      }
+    } else {
+      if (PWMLevel == 0) {
+        PWMLevel = 1;
+        increaseUp = true;
+      } else {
+        PWMLevel--;
+      }
+    }
+    break;
+  
+  case eLongPressFree:
+    Serial.println("Store PWM");
+    EEPROM.write(eepromAddrShim, PWMLevel);
+    break;
+
+  default: // eDoubleClick
+    Serial.println("Double click");
+    break;
+  }
+  
 }
